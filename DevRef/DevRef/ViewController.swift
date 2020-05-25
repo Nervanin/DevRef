798e0395c8ea014d7a68f1218657db4564c45b20
Alex <>
2020-05-25 22:21:58 +0300

    UIViewControlle

--- DevRef/DevRef/ViewController.swift
+++ DevRef/DevRef/ViewController.swift
@@ -8,30 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     
+    let patternsName = ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка","Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель","Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       tableView.dataSoure = self
     }
-
-}
-  let patternsName = ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка","Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель","Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"]
-extension UIViewController: UITableViewDataSource{
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return patternsName.count
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
-        
-        _ = patternsName[indexPath.row]
-        let patternsItem = patternsName[indexPath.row]
-
-        cell.textLabel?.text = patternsItem
-        cell.imageView?.image = UIImage(named: patternsItem)
+         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return patternsName.count
+        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
+    let patternsIteam = patternsName[indexPath.row]
+      
+      cell.textLabel?.text = patternsIteam
+      cell.imageView?.image = UIImage(named: patternsIteam)
         return cell
-    }
-    
-    
+        }
+
 }
