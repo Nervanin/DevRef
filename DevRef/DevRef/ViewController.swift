e265d1965771e6c98c76badc33b652ebb47e5d42
Alex <>
2020-06-04 00:46:20 +0300

    The list is divided into sections

--- DevRef/DevRef/ViewController.swift
+++ DevRef/DevRef/ViewController.swift
@@ -10,28 +10,47 @@ import UIKit
 class ViewController: UIViewController{
     
     let patternsName =
-    [
-        ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка"],
-        ["Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель"],
-        ["Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"]
-    ]
-
+        [
+            ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка"],
+            ["Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель"],
+            ["Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"]
+        ]
+    
+let headerTitles = ["Порождающие", "Структурные", "Поведенческие"]
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     }
 }
 
   extension ViewController: UITableViewDataSource{
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return patternsName.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-  
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
-    let patternsIteam = patternsName[indexPath.row]
-      cell.textLabel?.text = patternsIteam[indexPath.row]
-      //cell.imageView?.image = UIImage(named: patternsIteam)
-    return cell
-      }
-  }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return patternsName[section].count
+    }
+    
+    func numberOfSections(in: UITableView) -> Int {
+        return patternsName.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section < headerTitles.count {
+            return headerTitles[section]
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
+        
+        let patternsIteam = patternsName[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = patternsIteam
+        
+        cell.imageView?.image = UIImage(named: patternsIteam)
+        
+        return cell
+        
+    }
+    
+}
