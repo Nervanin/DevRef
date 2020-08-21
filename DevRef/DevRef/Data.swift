f2ce50da47c78fa2c669b1ef04699251856ec6d6
Alex <>
2020-08-21 16:57:28 +0300

    Data output through structure

---
+++ DevRef/DevRef/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  DevRef
+//
+//  Created by Alex on 10.07.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import Foundation
+
+struct Pattern {
+    var title: String
+    var names: [String]
+}
--- DevRef/DevRef/ViewController.swift
+++ DevRef/DevRef/ViewController.swift
@@ -9,48 +9,37 @@ import UIKit
 
 class ViewController: UIViewController{
     
-    let patternsName =
-        [
-            ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка"],
-            ["Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель"],
-            ["Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"]
-        ]
-    
-let headerTitles = ["Порождающие", "Структурные", "Поведенческие"]
+    let patterns = [Pattern(title: "Порождающие",
+                                 names: ["Фабричный метод","Абстрактная фабрика","Строитель","Прототип","Одиночка"]),
+                        Pattern(title: "Структурные",
+                                 names:["Адаптер","Мост","Компоновщик","Декоратор","Фасад","Легковес","Заместитель"]),
+                        Pattern(title: "Поведенческие",
+                                 names:  ["Цепочка обязанностей","Команда","Итератор","Посредник","Снимок","Наблюдатель","Состояние","Стратегия","Шаблонный метод","Посетитель"])]
     
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
     }
 }
 
   extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return patternsName[section].count
+        return patterns.map{$0}[section].names.count
     }
     
     func numberOfSections(in: UITableView) -> Int {
-        return patternsName.count
+        return patterns.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section < headerTitles.count {
-            return headerTitles[section]
-        }
-        return nil
+        return patterns.map{$0}[section].title
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
-        
-        let patternsIteam = patternsName[indexPath.section][indexPath.row]
-        
+        let patternsIteam = patterns.map{$0}[indexPath.section].names[indexPath.row]
         cell.textLabel?.text = patternsIteam
-        
         cell.imageView?.image = UIImage(named: patternsIteam)
-        
         return cell
         
     }
-    
 }
