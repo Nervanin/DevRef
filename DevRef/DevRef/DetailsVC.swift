22e00bce4b3fd85248ad926907822266a13797d9
Alex <>
2020-10-23 17:32:53 +0300

    2 label adjustment

--- DevRef/DevRef/DetailsVC.swift
+++ DevRef/DevRef/DetailsVC.swift
@@ -21,62 +21,27 @@ class DetailsVC: UIViewController {
         setupTableView2()
         self.background()
     }
-    
-    
+
     func background() {
         
         var imageView : UIImageView
         
-        
         imageView  = UIImageView()
         imageView.snp.makeConstraints { (make) in
             make.left.equalTo(view.snp.left).offset(100)
-            make.centerY.centerX.equalTo(self.view).inset(UIEdgeInsets(top: -420, left: 0, bottom: 0, right: 0))
+            make.centerY.centerX.equalTo(self.view).inset(UIEdgeInsets(top: -445, left: 0, bottom: 0, right: 0))
             make.width.height.equalTo(200)
     
-            
             imageView.image = UIImage(named:"Абстрактная фабрика.png")
             
-            
             self.view.addSubview(imageView)
-            
-            
-            
-//            (make) in
-//                  make.left.equalTo(view.snp.left).offset(100)
-//            make.centerY.centerX.equalTo(self.view)
-//                  make.width.height.equalTo(200)
         }
-        
-        
-        
-        
-        
-//    var imageView : UIImageView
-//        imageView  = UIImageView(frame:CGRect(x: 80, y: 170, width: 250, height: 250));
-//    imageView.image = UIImage(named:"Абстрактная фабрика.png")
-//    self.view.addSubview(imageView)
-
     }
-    
     func setupTableView() {
         view.addSubview(detailsLabel)
         detailsLabel.snp.makeConstraints{make in
             make.centerY.centerX.equalToSuperview().inset(UIEdgeInsets(top: -685, left: 100, bottom: 0, right: 100))
-        
         }
-        
-        
-        
-//        { (make) in
-//           make.centerY.centerX.equalToSuperview()
-//           make.height.width.equalTo(100)
-//       }
-//        detailsLabel.snp.makeConstraints { make in
-//            make.edges.equalTo(view).inset(UIEdgeInsets(top: -700, left: 100, bottom: 0, right: 104))
-//
-//        }
-        
         detailsLabel.numberOfLines = 0
         //метод отображает линии лайбле(по умолчанию стоит 1)/ставим 0 - делаем динамический текст
 //        detailsLabel.lineBreakMode = .byWordWrapping
@@ -99,50 +64,28 @@ class DetailsVC: UIViewController {
         
     }
     
-        func setupTableView2(){
-    view.addSubview(descriptionPattern)
-    descriptionPattern.snp.makeConstraints{make in
-        make.centerY.centerX.equalToSuperview().inset(UIEdgeInsets(top: -200, left: 0, bottom: 0, right: 0))
-
-    }
+    func setupTableView2(){
+        view.addSubview(descriptionPattern)
+                descriptionPattern.snp.makeConstraints { make in
+                    make.edges.equalTo(view).inset(UIEdgeInsets(top: 330, left: 15, bottom: 0, right: 15))
         
+                }
         
-        
-//        { make in
-//        make.edges.equalTo(view).inset(UIEdgeInsets(top: -700, left: 100, bottom: 0, right: 104))
-//
-//    }
-//
-        
-        
-        
-//        {make in
-//        make.centerY.centerX.equalToSuperview().inset(UIEdgeInsets(top: 400, left: 100, bottom: 0, right: 100))
-//
-//    }
-    
-    
         descriptionPattern.numberOfLines = 0
     //метод отображает линии лайбле(по умолчанию стоит 1)/ставим 0 - делаем динамический текст
         descriptionPattern.lineBreakMode = .byWordWrapping
     //переход слов на новую строку как именно
-        descriptionPattern.text = "IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER "
-        descriptionPattern.adjustsFontSizeToFitWidth = true
+        descriptionPattern.text = "IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS  IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER IOS DEVELOPER че ты тут забыл?"
+//        descriptionPattern.adjustsFontSizeToFitWidth = true
     //если больше текста чем вмешается в лайбл то уменьшается шрифт для вмешения текста
         descriptionPattern.sizeToFit()
     //показывает столько слов сколько есть в лайбле
         descriptionPattern.font = UIFont.boldSystemFont(ofSize: 14)
     //настройка размера шрифта
-        detailsLabel.textColor = UIColor.black
+        descriptionPattern.textColor = UIColor.black
     //цвет
-//        detailsLabel.shadowColor = UIColor.red
-    //окантовка текста
-//        detailsLabel.shadowOffset = CGSize(width: 0, height: 0)
-    //отступ тени
         descriptionPattern.center = self.view.center
     //настройка лейбла по центру
-        
-        
         
     }
         
